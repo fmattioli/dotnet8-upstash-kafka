9a3ce889294db82b6d4c929ec720acb8192f28c5
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-04-10 17:23:54 -0300

    First commit

---
+++ Upstash.Api.Producer/Upstash.Api.Producer/Controllers/ProducerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Confluent.Kafka;
+
+namespace Upstash.Api.Producer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProducerController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index([FromBody] string conteudoMensagem)
+        {
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = "helpful-starfish-14766-us1-kafka.upstash.io:9092",
+                SaslMechanism = SaslMechanism.ScramSha256,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslUsername = "aGVscGZ1bC1zdGFyZmlzaC0xNDc2NiQ_c-kKpftU9ZHCrcsixMfd0m-jpyksnD0",
+                SaslPassword = "NTlmMjRjMGMtNWIwYi00OWE5LTgyYTMtYTA4MTQ1MzY0ZWVk"
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+
+            await producer.ProduceAsync("topico-exemplo", new Message<Null, string> { Value = conteudoMensagem });
+            Console.WriteLine("Message sent successfully");
+
+            return Ok();
+        }
+    }
+}
---
+++ Upstash.Consumer.Kafka/Upstash.Consumer.Kafka/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "helpful-starfish-14766-us1-kafka.upstash.io:9092",
+    SaslMechanism = SaslMechanism.ScramSha256,
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslUsername = "aGVscGZ1bC1zdGFyZmlzaC0xNDc2NiQ_c-kKpftU9ZHCrcsixMfd0m-jpyksnD0",
+    SaslPassword = "NTlmMjRjMGMtNWIwYi00OWE5LTgyYTMtYTA4MTQ1MzY0ZWVk",
+    GroupId = "YOUR_CONSUMER_GROUP",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+
+consumer.Subscribe("topico-exemplo");
+
+while (true)
+{
+    var cr = consumer.Consume(TimeSpan.FromSeconds(1));
+    if (cr != null)
+    {
+        Console.WriteLine($"Event key {cr.Message.Key}, value {cr.Message.Value}");
+    }
+}
